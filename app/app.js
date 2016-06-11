c46cee11fb0e1b63f7791d5b4174bcd27b9486df
Sergey Guzhvay <feidos.gs@gmail.com>
2016-06-11 07:48:39 +0300

    improved interface, active tabs/buttons

--- app/app.js
+++ app/app.js
@@ -1,14 +1,27 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+(function () {
+    var app = angular.module('makeupApp', []);
+    app.controller('MainController', function () {
+        var mc = this;
+        mc.selectedTab = 0;
+        
+        mc.menuButtons = [
+            {tab: 1, name: 'Face', size: 2},
+            {tab: 2, name: 'Eyes', size: 2},
+            {tab: 3, name: 'Lips', size: 2},
+            {tab: 4, name: 'Looks Natura', size: 2},
+            {tab: 5, name: 'My Gallery', size: 2}
+        ];
+        mc.previewButtons = [
+            {name: 'Remove all', size: 3},
+            {name: 'Before/After', size: 3},
+            {name: 'Share', size: 2},
+            {name: 'What is used', size: 4}
+        ];
+        mc.selectTab = function (n) {
+            mc.selectedTab = n;
+        }
+    });
+})();
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
--- app/js/resize.js
+++ app/js/resize.js
@@ -1,14 +1,25 @@
 var resize = function () {
+
     var mainDiv = document.getElementById('main-div');
     var topSpace = document.getElementById('top-space');
+    var menu = document.getElementById('menu');
+    var menuBtns = document.getElementsByClassName('menu-btn-body');
+
     var appWidth = window.innerWidth * 0.9;
-    if (appWidth > 1400) appWidth = 1400;
+    if (appWidth > 1400) appWidth = 1500;
     else if (appWidth < 800) appWidth = 800;
     var appHeight = appWidth * 0.585;
     var margin = (window.innerHeight - appHeight) / 3;
+
+    menu.style.fontSize = Math.round(appWidth / 64) + 'px';
     mainDiv.style.width = appWidth + 'px';
     mainDiv.style.height = appHeight + 'px';
-    topSpace.style.height = margin > 0 ? margin + 'px' : 0;
+    // topSpace.style.height = margin > 0 ? margin + 'px' : 0;
+    for (var n in menuBtns) {
+        var btn = menuBtns[n];
+        if (!btn.style) continue;
+        btn.style.borderBottomWidth = Math.round((appWidth - 400) / 200) + 1 + 'px';
+    }
 };
 
 window.onload = resize;
